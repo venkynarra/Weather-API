801048a189224b37ac5e73130def57e99247d5e0
venkatesh <venkateshnarra368@gmail.com>
2025-05-07 13:08:40 -0500

    Day31= show stations on the home page

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import pandas
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows = 17)
+stations = stations[["STAID","STANAME"]]
 
 @app.route("/") #decarator, it connect below method to this function
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data = stations.to_html())
 
 @app.route("/api/v1/<station>/<date>") #decarator, it connect below method to this function
 def about(station, date):
-    df = pandas.read_csv("")
-    temperature = df.station(date)
-    return render_template("about.html")
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows = 20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 if __name__ == "__main__":
     app.run(debug=True)
