d924089b44b1c02fc35facb1a27475f212246d81
venkatesh <venkateshnarra368@gmail.com>
2025-05-07 10:37:15 -0500

    Day29- Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import pandas
 from flask import Flask, render_template
 
-app = Flask("website")
+app = Flask(__name__)
 
-@app.route("/home") #decarator, it connect below method to this function
+
+@app.route("/") #decarator, it connect below method to this function
 def home():
-    return render_template("tutorial.html")
-@app.route("/about/") #decarator, it connect below method to this function
-def about():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>") #decarator, it connect below method to this function
+def about(station, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
     return render_template("about.html")
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
